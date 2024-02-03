87c92c5221e392d57be2809cf649a60574ee16d7
Alaincustic <147705200+Alaincustic@users.noreply.github.com>
2024-02-03 20:45:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const path = require("path");
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
-// Apply JSON body parsing middleware
-app.use(express.json());
+// List of allowed frontend origins for CORS
+const allowedOrigins = [
+  "https://thaibasiljp.com/",
+  "http://thaibasiljp.com/",
+  "https://thaibasiljp.com",
+  "http://thaibasiljp.com",
+  "http://127.0.0.1:5501",
+  "http://127.0.0.1:5500",
+];
 
-// CORS configuration and allowed origins remain the same
+// CORS configuration
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -21,23 +28,25 @@ const corsOptions = {
 // Apply the CORS middleware
 app.use(cors(corsOptions));
 
-// Your route definition remains the same
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Route to handle the POST request
 app.post(
   "/",
   (req, res, next) => {
     const referer = req.headers.referer;
+
     // Check if the referer exists in the allowedReferrers array
-    if (
-      referer &&
-      allowedReferrers.some((domain) => referer.startsWith(domain))
-    ) {
+    if (allowedOrigins.includes(referer)) {
       next();
     } else {
-      res.status(403).send("Access forbidden");
+      res.status(403).send("Access forbidden due to invalid referer.");
     }
   },
   (req, res) => {
-    const { timezone } = req.body; // Now req.body should be properly defined
+    const { timezone } = req.body;
+
     if (timezone === "Asia/Tokyo") {
       res.sendFile(path.join(__dirname, "altmod.html"));
     } else {
