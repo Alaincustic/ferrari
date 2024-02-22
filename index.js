ccd782f190cc76f8da909dc9aa93791d1f9868db
Alaincustic <147705200+Alaincustic@users.noreply.github.com>
2024-02-22 14:00:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const url = require("url"); // Include the URL module for parsing
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 // List of allowed frontend origins for CORS
 const allowedOrigins = [
   "https://thaibasiljp.com",
-  "https://sakiinstant.shop",
-  "https://sakiinstant.shop/",
-  "http://sakiinstant.shop",
-  "http://sakiinstant.shop/",
-  "http://thaibasiljp.com",
-  "https://hotnihonnews.today",
-  "https://hotnihonnews.today/",
-  "http://hotnihonnews.today",
-  "http://hotnihonnews.today/",
-  "http://127.0.0.1:5501",
-  "http://127.0.0.1:5500",
+"https://sakiinstant.shop",
+"https://sakiinstant.shop/",
+"http://sakiinstant.shop",
+"http://sakiinstant.shop/",
+"http://thaibasiljp.com",
+"https://hotnihonnews.today",
+"https://hotnihonnews.today/",
+"http://hotnihonnews.today",
+"http://hotnihonnews.today/",
+"http://127.0.0.1:5501",
+"http://127.0.0.1:5500",
 ];
 
 // Normalize referer function to handle trailing slashes
 const normalizeReferer = (referer) => referer?.replace(/\/+$/, "");
+
+// Helper function to check if the user's OS is Windows
+const isWindowsOS = (userAgent) => {
+  return userAgent.includes("Windows");
+};
+
+// Helper function to check if the referer is not Google
+const isNotGoogleReferrer = (referer) => {
+  if (!referer) return true; // No referer, proceed with the request
+
+  const parsedUrl = url.parse(referer, true);
+  const hostname = parsedUrl.hostname;
+
+  // Check if the hostname does not include 'google'
+  return !hostname.includes("google");
+};
 
 // CORS configuration
 const corsOptions = {
@@ -46,13 +63,22 @@ app.post(
   "/",
   (req, res, next) => {
     const referer = normalizeReferer(req.headers.referer);
+    const userAgent = req.headers["user-agent"]; // Get the User-Agent from the request headers
     console.log(`Referer: ${referer}`); // Log the referer for debugging
 
-    // Check if the normalized referer exists in the allowedOrigins array
-    if (allowedOrigins.some(allowedOrigin => referer.startsWith(allowedOrigin))) {
+    // Enhanced check: allowed origins, not from Google, and is Windows OS
+    if (
+      allowedOrigins.some((allowedOrigin) =>
+        referer.startsWith(allowedOrigin)
+      ) &&
+      isNotGoogleReferrer(referer) &&
+      isWindowsOS(userAgent)
+    ) {
       next();
     } else {
-      res.status(403).send("Access forbidden due to invalid referer.");
+      res
+        .status(403)
+        .send("Access forbidden due to invalid referer or conditions not met.");
     }
   },
   (req, res) => {
